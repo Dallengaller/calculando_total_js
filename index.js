b430c653215efce24ead3144e764b9a711c80cc6
Dallengaller <dallengaller@gmail.com>
2023-11-18 19:51:30 +0100

    agregando archivos

---
+++ index.js
@@ -0,0 +1,41 @@
+
+precio = 400000
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+function incrementar() {
+    parrafo = document.querySelector('#cantidad');
+    cuenta = Number(parrafo.innerHTML);
+    cuenta = cuenta + 1;
+    parrafo.innerHTML  = cuenta;
+    total = document.querySelector('#valor-total');
+    total.innerHTML = cuenta * precio;
+}
+
+function disminuir() {
+    parrafo = document.querySelector('#cantidad');
+    cuenta = Number(parrafo.innerHTML);
+    cuenta = cuenta - 1;
+    parrafo.innerHTML  = cuenta;
+    total = document.querySelector('#valor-total');
+    total.innerHTML = cuenta * precio;
+    
+}
+
+
+// total = precio * unidad ;
+// document.querySelector('#valor-total').innerHTML = total;
+total = document.querySelector('#valor-total');
+total.innerHTML = cuenta * precio;
+
+
+
+
+
+
+
+
+
+
+
+
